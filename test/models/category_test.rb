f2d6ccd7815f6c77bce9addfe0d81e0e06e4d352
Kauana Tombolato <kauanatomb@gmail.com>
2024-09-08 15:43:14 -0300

    test for model category

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save valid category" do
+    category = Category.new(name: "Category test")
+    assert category.save, "Failed to save valid category"
+  end
+
+  test "should not save unvalid category" do
+    category = Category.new(name: "")
+    assert_not category.save, "Saved the category with an empty name"
+    assert_not_empty category.errors[:name], "No validation error for name present"
+  end
+
+  test "should not save duplicate name category" do
+    category = Category.new(name: "Category test")
+    category1 = Category.new(name: "Category test")
+    assert category.save, "Failed to save valid category"
+    assert_not category1.save, "Saved the category with repeatable name"
+  end
+
+  test "should have many ingredient" do
+    category = Category.reflect_on_association(:ingredients)
+    assert_equal(category.macro, :has_many, "Category does not have a has_many association with ingredients") 
+  end
 end
